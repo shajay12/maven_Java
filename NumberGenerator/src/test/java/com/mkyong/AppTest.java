e4cde8a7c223f2f23ce5745e2e2a15e676a1edf2
shajay12 <sh.ajay12@gmail.com>
2020-06-17 13:11:49 +0530

    made changes to test file

--- NumberGenerator/src/test/java/com/mkyong/AppTest.java
+++ NumberGenerator/src/test/java/com/mkyong/AppTest.java
@@ -5,21 +5,6 @@ import org.junit.Test;
 
 public class AppTest {
 
-	    public boolean isEvenNumber(int number){
-         
-        boolean result = false;
-        if(number%2 == 0){
-            result = true;
-        }
-        return result;
-    }
-     
-    @Test
-    public void evenNumberTest(){
-        MyAssertTrueTest asft = new MyAssertTrueTest();
-        assertTrue(asft.isEvenNumber(4));
-    }
-	
 	@Test
 	public void testLengthOfTheUniqueKey() {
 
