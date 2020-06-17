7b2cc12370a982a2cad7a586c2c94a955121c9ef
shajay12 <sh.ajay12@gmail.com>
2020-06-17 12:57:19 +0530

    made changes to test file

--- NumberGenerator/src/test/java/com/mkyong/AppTest.java
+++ NumberGenerator/src/test/java/com/mkyong/AppTest.java
@@ -1,6 +1,6 @@
 package com.mkyong;
 
-import org.junit.Assert.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
@@ -9,12 +9,6 @@ public class AppTest {
 	public void testLengthOfTheUniqueKey() {
 
 		App obj = new App();
-Assert.assertEquals(36, obj.generateUniqueKey().length());
-	}
-	@Test
-	public void sampletestpass() {
-		int a = 1;
-		int b = 2;
-assertTrue(a + b == 3);
+		Assert.assertEquals(36, obj.generateUniqueKey().length());
 	}
 }
